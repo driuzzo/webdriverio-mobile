5b40c2542f5cd152a2caae251bca4878716f3545
adriano <adriano.driuzzo@hotmail.com>
2025-07-26 11:39:53 -0300

    appId vai pra common capabilities

--- config/wdio.bs.conf.js
+++ config/wdio.bs.conf.js
@@ -8,7 +8,7 @@ exports.config = {
     [
       'browserstack',
       {
-        app: 'bs://64f11b62079534c89e86df8943c4327ad13ce8ba',
+        //app: 'bs://64f11b62079534c89e86df8943c4327ad13ce8ba',
         buildIdentifier: "12.0",
         browserstackLocal: true
       },
@@ -16,7 +16,6 @@ exports.config = {
   ],
   capabilities: [{
     'bstack:options': {
-      //app: process.env.BROWSERSTACK_APP_ID,
       deviceName: 'Xiaomi Redmi Note 11',
       platformVersion: '11.0',
       platformName: 'android',
@@ -36,6 +35,7 @@ exports.config = {
   }],
   commonCapabilities: {
     'bstack:options': {
+      app: process.env.BROWSERSTACK_APP_ID,
       projectName: "BrowserStack Samples",
       buildName: 'browserstack build',
       sessionName: 'BStack parallel webdriverio-appium',
